a3d13c0a9fe9524f30baa1705ff66ad9a5efee91
Scottyp1 <codymoto122@gmail.com>
2026-03-11 13:41:06 -0400

    feat(login): designed login page, login form and the auth shell

---
+++ src/components/auth/AuthCTA.tsx
@@ -0,0 +1,18 @@
+const AuthCTA = () => {
+  return (
+    <div className="auth-login-img w-1/2 px-6 flex-col items-center justify-center hidden md:flex gap-2">
+      <span className="text-gray-500 font-bold text-md tracking-[5px] self-start">
+        WELCOME BACK
+      </span>
+      <h1 className="self-start text-4xl font-bold italic">
+        Study Smarter. Not Harder.
+      </h1>
+      <span className="text-gray-400">
+        Organize lectures, manage assignments, and track your academic progress
+        in one streamlined workspace.
+      </span>
+    </div>
+  )
+}
+
+export default AuthCTA
---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,50 @@
+import { Link } from '@tanstack/react-router'
+import GlassButton from '../liquidGlass/GlassButton'
+import GlassInput from '../liquidGlass/GlassInput'
+
+import { FaApple } from 'react-icons/fa'
+
+const LoginForm = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-12 px-6 w-full">
+      <h2 className="text-3xl font-bold self-start">Sign in</h2>
+      <div className="flex flex-col gap-4 w-full">
+        <section className="flex flex-col gap-4">
+          <GlassButton
+            label="Sign in with Apple"
+            className=""
+            icon={<FaApple size={30} />}
+          />
+          <GlassButton
+            label="Sign in with Google"
+            icon={<img src="/images/google-icon.png" width={30} />}
+          />
+        </section>
+        <section className="flex gap-4 items-center justify-center">
+          <div className="w-full bg-gray-500 h-px" />
+          <span>or</span>
+          <div className="w-full bg-gray-500 h-px" />
+        </section>
+        <section className="flex flex-col gap-4 w-full">
+          <GlassInput placeholder="Email" />
+          <GlassInput placeholder="Password" />
+          <button className="bg-white text-black rounded-2xl p-4 font-bold">
+            Sign in
+          </button>
+        </section>
+        <section className="flex flex-col gap-2 justify-center items-center">
+          <span>Forgot Password?</span>
+          <span className="text-white/40">
+            No account?
+            <Link to="." className="text-white">
+              {' '}
+              Click here
+            </Link>
+          </span>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
